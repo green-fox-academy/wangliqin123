87577b1fcc80a15b50d75b2049a0e563ae94936d
Lorand <szilagyi.lori@gmail.com>
2017-09-11 23:03:52 +0200

    exercise19 done

---
+++ week-02/day-01/exercise19/OneTwoALot/OneTwoALot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var input = Console.ReadLine();
+            Int32.TryParse(input, out int i);
+
+            if (i <= 0)
+            {
+                Console.WriteLine("Not enough");
+            }
+
+            else if (i == 1)
+            {
+                Console.WriteLine("One");
+            }
+
+            else if (i == 2)
+            {
+                Console.WriteLine("Two");
+            }
+
+            else
+            {
+                Console.WriteLine("A lot");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
