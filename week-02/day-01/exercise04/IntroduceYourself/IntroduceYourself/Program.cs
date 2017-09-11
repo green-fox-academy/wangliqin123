589e4cf783bb54c4b01370f31854215aba2ec74d
Lorand <szilagyi.lori@gmail.com>
2017-09-11 13:29:11 +0200

    exercise04 done

---
+++ week-02/day-01/exercise04/IntroduceYourself/IntroduceYourself/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loránd Szilágyi");
+            Console.WriteLine("30");
+            Console.WriteLine("1.8");
+            Console.ReadLine();
+        }
+    }
+}
