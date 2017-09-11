df86a7864596bc96f3f62194cc46f4a0a98531e8
Lorand <szilagyi.lori@gmail.com>
2017-09-11 18:58:50 +0200

    exercise19 done

---
+++ week-02/day-01/exercise18/OddEven/OddEven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            Console.WriteLine("enter a number");
+
+            while (int.TryParse(Console.ReadLine(), out i))
+            {
+                Console.WriteLine(i % 2 == 0 ? "even" : "odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
