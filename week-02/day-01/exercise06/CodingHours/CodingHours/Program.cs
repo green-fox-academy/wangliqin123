91103b3acd4a23d6f10291733bb75bea046f517d
Lorand <szilagyi.lori@gmail.com>
2017-09-11 14:29:13 +0200

    exercise06 done

---
+++ week-02/day-01/exercise06/CodingHours/CodingHours/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(17 * 5 * 6);
+            Console.WriteLine(510 / (17.0 * 52) * 100);
+           
+            Console.ReadLine();
+        }
+    }
+}
