995314ae38bfbc689868c9283a3cb417fb7f6b1a
Lorand <szilagyi.lori@gmail.com>
2017-09-11 17:25:48 +0200

    exercise14 done

---
+++ week-02/day-01/exercise14/hellouser/hellouser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Enter your name:";
+            string name;
+
+            Console.Write(message);
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name + "!");
+
+            Console.ReadLine();    
+        }
+    }
+}
