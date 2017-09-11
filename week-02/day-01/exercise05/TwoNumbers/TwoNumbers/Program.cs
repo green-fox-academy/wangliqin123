4309abab24db8119f957d2987ece96e362dd77a6
Lorand <szilagyi.lori@gmail.com>
2017-09-11 13:52:08 +0200

    exercise05 done

---
+++ week-02/day-01/exercise05/TwoNumbers/TwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(13 + 22);
+            Console.WriteLine(22 - 13);
+            Console.WriteLine(22 * 13);
+            Console.WriteLine(22.0 / 13);
+            Console.WriteLine(22 / 13);
+            Console.WriteLine(22 % 13);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
