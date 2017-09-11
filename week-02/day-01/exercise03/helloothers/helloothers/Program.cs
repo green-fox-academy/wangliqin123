682873dd02696cd058dbe27b1e81cdcc291f852a
Lorand <szilagyi.lori@gmail.com>
2017-09-11 13:15:47 +0200

    exercise03 done

---
+++ week-02/day-01/exercise03/helloothers/helloothers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Bazsi!");
+            Console.WriteLine("Hello, Berci!");
+            Console.WriteLine("Hello, Vili!");
+            Console.ReadLine();
+        }
+    }
+}
