6027f7c0eb60c48b0e6a8ff5f6de2d7e2745ddd5
Lorand <szilagyi.lori@gmail.com>
2017-09-11 13:56:37 +0200

    exercise05 done

--- week-02/day-01/exercise05/TwoNumbers/TwoNumbers/Program.cs
+++ week-02/day-01/exercise05/TwoNumbers/TwoNumbers/Program.cs
@@ -9,9 +9,10 @@ namespace GreenFox
             Console.WriteLine(13 + 22);
             Console.WriteLine(22 - 13);
             Console.WriteLine(22 * 13);
-            Console.WriteLine(22.0 / 13);
+            Console.WriteLine(22.0 / 13.0);
             Console.WriteLine(22 / 13);
             Console.WriteLine(22 % 13);
+          
 
             Console.ReadLine();
             
