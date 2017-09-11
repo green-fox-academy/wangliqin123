ba50e43fc2a9efd64c47c75116ec4012bfd7022c
Lorand <szilagyi.lori@gmail.com>
2017-09-11 16:02:02 +0200

    exercise10 done

---
+++ week-02/day-01/exercise10/DefineBasicInfo/DefineBasicInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lorand";
+            int age = 30;
+            double height = 1.8;
+            bool married = false;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+
+            if (married)
+            {
+                Console.WriteLine("married");
+            }
+
+            else
+            {
+                Console.WriteLine("not married");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
