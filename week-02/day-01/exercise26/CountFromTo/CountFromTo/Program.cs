6da4b88175bd46887a0d593ef3461e31bfb54a44
Lorand <szilagyi.lori@gmail.com>
2017-09-11 23:59:36 +0200

    exercise26 done

---
+++ week-02/day-01/exercise26/CountFromTo/CountFromTo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int num = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter number: ");
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            if (num2 < num)
+            {
+                
+                Console.WriteLine("The second number should be bigger");
+            }
+
+            for (int i = num; i < num2; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
