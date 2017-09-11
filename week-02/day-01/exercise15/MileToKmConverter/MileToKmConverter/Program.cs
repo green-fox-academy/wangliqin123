ac707d368cbf93215bd811b89afa3699259a3de5
Lorand <szilagyi.lori@gmail.com>
2017-09-11 18:37:23 +0200

    exercise15 done

---
+++ week-02/day-01/exercise15/MileToKmConverter/MileToKmConverter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter distance in kilometers:");
+
+            string input = Console.ReadLine();
+
+            double number = Convert.ToInt32(input);
+            double result = (double) (number * 0.65);
+
+            Console.Write(number + result + "miles");
+
+            Console.ReadLine();
+        }
+    }
+}
