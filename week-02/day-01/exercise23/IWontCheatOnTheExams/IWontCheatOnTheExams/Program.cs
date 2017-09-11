a53812659f8b577732087ce58dc49a63f21476b8
Lorand <szilagyi.lori@gmail.com>
2017-09-11 23:20:10 +0200

    exercise23 done

---
+++ week-02/day-01/exercise23/IWontCheatOnTheExams/IWontCheatOnTheExams/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("I won't cheat on the exam!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
