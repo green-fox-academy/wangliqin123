5cb30ffc64d304b8daa888b2ddfbf848e9c38215
Lorand <szilagyi.lori@gmail.com>
2017-09-11 15:43:47 +0200

    exercise10 done

---
+++ week-02/day-01/exercise09/bmi/bmi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+
+            Console.WriteLine(massInKg / (heightInM * heightInM));
+
+            Console.ReadLine();
+        }
+    }
+}
