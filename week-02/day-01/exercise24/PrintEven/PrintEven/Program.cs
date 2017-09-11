faa0d95bc6a9501104c223c6118cd416618039c6
Lorand <szilagyi.lori@gmail.com>
2017-09-11 23:32:34 +0200

    exercise24 done

---
+++ week-02/day-01/exercise24/PrintEven/PrintEven/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                for (int i = 0; i < 502; i += 2) Console.WriteLine(i);
+                Console.ReadLine();
+            }          
+        }
+    }
+}
