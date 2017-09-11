cc77e08dc7460e41c9699ca55230c017f689218c
Lorand <szilagyi.lori@gmail.com>
2017-09-11 15:30:49 +0200

    exercise08 done

---
+++ week-02/day-01/exercise08/swap/swap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
