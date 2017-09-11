f83e224b52bb3e200a755d52b0134ae920b159de
Lorand <szilagyi.lori@gmail.com>
2017-09-11 23:44:44 +0200

    exercise25 done

---
+++ week-02/day-01/exercise25/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == 0)
+                return;
+
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine("{0} * {1} ={2}", n, i, n * i);
+
+            Console.ReadLine();
+        }
+    }
+}
