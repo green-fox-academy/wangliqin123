8601d4658d2a73402d71977a2d07e4bf313deebf
Lorand <szilagyi.lori@gmail.com>
2017-09-12 17:02:50 +0200

    exercise09 done

---
+++ week-02/day-02/exercise09/SumElements/SumElements/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r = { 54, 23, 66, 12 };
+
+            Console.WriteLine(r[1] + r[2]);
+            
+            Console.ReadLine();
+        }
+    }
+}
