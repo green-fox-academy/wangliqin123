ecbdf4421153c0e688c35cf0a8a007d7b8f51c35
Lorand <szilagyi.lori@gmail.com>
2017-09-12 17:20:34 +0200

    exercise11 done

---
+++ week-02/day-02/exercise11/IncrementElement/IncrementElement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(++ t[2]);
+           
+            Console.ReadLine();
+        }
+    }
+}
