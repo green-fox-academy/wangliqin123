5ad038f2c64eea0cda6de75926abcaf7ef967a05
Lorand <szilagyi.lori@gmail.com>
2017-09-12 23:03:22 +0200

    exercise16 done

---
+++ week-02/day-02/exercise16/Append A/Append A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nimals = { "kuty", "macsk", "cic" };
+
+            for (int i = 0; i < nimals.Length; i++)
+            {
+                Console.WriteLine(nimals[i] + "a");
+            }
+            Console.ReadLine();
+        }
+    }
+}
