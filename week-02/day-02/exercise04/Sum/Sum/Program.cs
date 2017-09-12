b2b34565a82d027090e47a8192f0e8b7475bfac9
Lorand <szilagyi.lori@gmail.com>
2017-09-12 16:26:27 +0200

    exercise07 done

---
+++ week-02/day-02/exercise04/Sum/Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3;
+            int num2 = 4;
+            int num3 = 5;
+            sum (num1, num2, num3);
+            Console.ReadKey();
+            
+            Console.ReadLine();
+        }
+
+        public static void sum(int num1, int num2, int num3)
+        {
+            Console.WriteLine(num1 + num2 + num3);
+
+        }
+    }
+}
---
+++ week-02/day-02/exercise07/Third/Third/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] q = { 4, 5, 6, 7 };
+
+            Console.WriteLine(q[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
