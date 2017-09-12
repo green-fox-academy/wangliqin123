68c5d4265097ef2f8101e456c6f6c958d0e8e689
Lorand <szilagyi.lori@gmail.com>
2017-09-12 23:48:57 +0200

    exercise18 done

---
+++ week-02/day-02/exercise17/Swap Elements/Swap Elements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] abc = { "first", "second", "third" };
+        }   
+            public static void Swapstrings(string[] abc, string[0], string[2])
+            {   string temp = abc[0];
+                abc[0] = abc[2];
+                abc[2] = temp;
+
+
+
+                Console.ReadLine();
+            }  
+            
+        
+    }
+}
---
+++ week-02/day-02/exercise18/SumAll/SumAll/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ai = { 3, 4, 5, 6, 7 };
+
+            for (int i = 1; i < ai.Length; i++)
+            {
+                ai[0] += ai[i];
+            }
+            Console.WriteLine(ai[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
