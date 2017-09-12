dfd0bbfb4ccb397818eea1099db66c6b56e959e7
Lorand <szilagyi.lori@gmail.com>
2017-09-12 13:40:25 +0200

    exercise02 done

---
+++ week-02/day-02/exercise02/Greet/Greet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+            greet(al);
+            Console.ReadLine();
+        }
+
+        public static void greet(string al)
+        {
+             Console.WriteLine("Greetings dear, " + al);
+        }
+            
+            
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+            
+    }
+}
