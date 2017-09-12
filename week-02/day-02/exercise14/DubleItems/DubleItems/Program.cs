6e35ffbd3b5020e2f791d4fef3b505f8c7a40e21
Lorand <szilagyi.lori@gmail.com>
2017-09-12 22:53:54 +0200

    exercise14 done

---
+++ week-02/day-02/exercise14/DubleItems/DubleItems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ag = { 3, 4, 5, 6, 7 };
+            
+            for (int i = 0; i < ag.Length; i++)
+            {   
+             Console.WriteLine(ag[i] *= 2);   
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
