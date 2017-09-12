83a8ded37613e27cbde8f3c6630928994a168c48
Lorand <szilagyi.lori@gmail.com>
2017-09-13 00:00:50 +0200

    exercise19 began

---
+++ week-02/day-02/exercise19/Reverse/Reverse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aj = { 3, 4, 5, 6, 7 };
+            
+            Console.ReadLine();
+        }
+    }
+}
