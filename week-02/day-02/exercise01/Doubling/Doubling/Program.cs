6a027f261e5994f163a014816c12f3b4decac470
Lorand <szilagyi.lori@gmail.com>
2017-09-12 13:18:15 +0200

    exercise01 done

---
+++ week-02/day-02/exercise01/Doubling/Doubling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            doubling (ak);
+            Console.ReadKey();
+        }
+ 
+        public static void doubling(int ak)
+        {
+            Console.WriteLine(ak * 2);
+        }
+
+        // - Create an integer variable named `ak` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter
+        // - Print the result of `doubling(ak)`
+
+
+    }
+}
