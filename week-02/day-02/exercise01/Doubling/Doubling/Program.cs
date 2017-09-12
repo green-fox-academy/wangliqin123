c62f785fb91b5ae2efc4f32eb6641ae40fc8a57f
Lorand <szilagyi.lori@gmail.com>
2017-09-12 13:19:15 +0200

    exercise01 full

--- week-02/day-02/exercise01/Doubling/Doubling/Program.cs
+++ week-02/day-02/exercise01/Doubling/Doubling/Program.cs
@@ -15,11 +15,5 @@ namespace Doubling
         {
             Console.WriteLine(ak * 2);
         }
-
-        // - Create an integer variable named `ak` and assign the value `123` to it
-        // - Create a function called `doubling` that doubles it's input parameter
-        // - Print the result of `doubling(ak)`
-
-
     }
 }
