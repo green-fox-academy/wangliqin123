9b0405e92ef92927ac2aa8d79aa1664e77e6f8f0
Lorand <szilagyi.lori@gmail.com>
2017-09-12 16:54:06 +0200

    exercise08 done

---
+++ week-02/day-02/exercise08/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] p1 = { 1, 2, 3};
+            int[] p2 = { 4, 5 };
+
+            Console.WriteLine(p1.Length < p2.Length);
+        
+            Console.ReadLine();
+        }  
+    }
+}
