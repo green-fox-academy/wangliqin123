f6851fbe7d4f96f9557bb622075f64db71c75c21
Lorand <szilagyi.lori@gmail.com>
2017-09-12 17:14:19 +0200

    exercise10 done

---
+++ week-02/day-02/exercise10/ChangeElement/ChangeElement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChangeElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] s = { 1, 2, 3, 8, 5, 6 };
+            s[3] = 4;
+            Console.WriteLine(s[3]);
+           
+            Console.ReadLine();
+        }
+    }
+}
