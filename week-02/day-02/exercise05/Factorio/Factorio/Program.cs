7d8720288aeaa308196c729eaca134694aeab20f
Lorand <szilagyi.lori@gmail.com>
2017-09-12 22:09:24 +0200

    exercise05 done

---
+++ week-02/day-02/exercise05/Factorio/Factorio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            long fact = Factorio(number);
+            Console.WriteLine("{0}! = {1}", number, fact);
+            Console.ReadKey();
+        }
+        public static long Factorio(int number)
+        {
+            if (number == 0)
+
+            {
+                return 1;
+            }
+            return number * Factorio(number - 1);
+        }
+    }
+}
---
+++ week-02/day-02/exercise13/DiagonalMatrix/DiagonalMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            matrix();
+            Console.ReadLine();
+        }
+
+        static void matrix()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j) Console.Write("1 ");
+                    else Console.Write("0 ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
