683a3e16fc64250d6858af96dbdec80a1654eac1
Lorand <szilagyi.lori@gmail.com>
2017-09-12 09:45:53 +0200

    exercise21 part of it done

---
+++ week-02/day-01/exercise21/PartyIndicator/PartyIndicator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int num = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("girls number: " + num);
+
+            Console.Write("Enter number: ");
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("boys number: " + num2);
+
+            Console.WriteLine("The party is excellent!");
+
+            if (num = num2)  num + num2 > 20);
+            {
+                Console.WriteLine("Quite cool party!");
+            }
+
+            Console.ReadLine();
+            
+
+           
+            // If the the number of girls and boys are equal and there are more people coming than 20
+            //
+            // It should print: Quite cool party!
+            // It there are more than 20 people coming but the girl - boy ratio is not 1-1
+            //
+            // It should print: Average party...
+            // If there are less people coming than 20
+            //
+            // It should print: Sausage party
+            // If no girls are coming, regardless the count of the people
+        }
+    }
+}
---
+++ week-02/day-01/exercise28/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = Console.ReadLine();
+            Int32.TryParse(input, out int num);
+
+            Console.ReadLine();
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+        }
+    }
+}
