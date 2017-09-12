4b4b088125460850c5ca22eb335549c5c0ac8c0e
Lorand <szilagyi.lori@gmail.com>
2017-09-12 17:25:36 +0200

    exercise12 done

---
+++ week-02/day-02/exercise12/PrintAll/PrintAll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = { 4, 5, 6, 7 };
+            for (int i = 0; i < af.Length; i++)
+            { 
+                Console.WriteLine(af[i]);
+            } 
+            Console.ReadLine();
+        }
+    }
+}
