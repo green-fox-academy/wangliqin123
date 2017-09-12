f68b217cfc74c3b0e598dfd6e252e1a009b56b35
Lorand <szilagyi.lori@gmail.com>
2017-09-12 13:52:25 +0200

    exercise03 done

---
+++ week-02/day-02/exercise03/Append A/Append A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+            appendA(am);
+            Console.ReadLine();
+        }
+        public static void appendA(string am)
+        {
+            Console.WriteLine(am + "a");
+        }  
+    }
+}
