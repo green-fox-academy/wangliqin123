cf52b633ede465ce4d1e88fd936ce5f9e3baa5d4
Lorand <szilagyi.lori@gmail.com>
2017-09-13 16:22:25 +0200

    strings05 done

---
+++ week-02/day-03/strings05/Reverse/Reverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Reverse
+{
+    public class Reverse
+    {
+        public static void Main(string[] args)
+        {
+
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            string updatereversed = "";
+
+            for (int i=reversed.Length - 1; i >=0; i--)
+            {
+                updatereversed = updatereversed + reversed[i];
+            }
+            Console.WriteLine(updatereversed);
+            Console.ReadLine();
+        }
+    }
+}
