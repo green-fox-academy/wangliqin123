9e3a633ffeafdcb88ac91724bbe28659174b55b1
Lorand <szilagyi.lori@gmail.com>
2017-09-13 11:41:40 +0200

    strings01 done

---
+++ week-02/day-03/strings01/SimpleReplace/SimpleReplace/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleReplace
+{
+    public class SimpleReplace
+    {
+        public static void Main(string[] args)
+        {
+            string example = "In a dishwasher far far away";
+
+            Console.WriteLine(example.Replace("dishwasher", "galaxy"));
+
+            Console.ReadLine();
+        }
+    }
+}
