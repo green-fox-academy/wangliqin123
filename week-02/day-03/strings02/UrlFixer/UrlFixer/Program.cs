8156b73b4023494abdaa731e0054e20252071fb3
Lorand <szilagyi.lori@gmail.com>
2017-09-13 11:50:49 +0200

    strings02 done

---
+++ week-02/day-03/strings02/UrlFixer/UrlFixer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UrlFixer
+{
+    public class UrlFixer
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            
+            url = (url.Replace("bots" , "odds"));
+            Console.WriteLine(url.Insert(5, ":"));
+
+            Console.ReadLine();
+        }
+    }
+}
