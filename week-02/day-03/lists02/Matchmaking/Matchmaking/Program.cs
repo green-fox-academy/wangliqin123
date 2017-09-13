bfc55a1906f21cc53cb22ba5f6c41359e19318bd
Lorand <szilagyi.lori@gmail.com>
2017-09-13 17:33:05 +0200

    lists02 basic

---
+++ week-02/day-03/lists02/Matchmaking/Matchmaking/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matchmaking
+{
+    public class Matchmaking
+    {
+        public static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string>();
+
+            //girls.Concat(boys);
+                //order.AddRange(girls);
+            //order.AddRange(boys);
+            ////girls.Count + boys.Count;
+
+
+            //order = girls.Concat(boys).ToList();
+
+            Console.WriteLine(girls);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
