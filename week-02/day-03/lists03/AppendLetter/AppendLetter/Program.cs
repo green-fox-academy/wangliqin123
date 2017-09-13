2f0dc8dfe06100f5acc78177168ad6e2ebef1185
Lorand <szilagyi.lori@gmail.com>
2017-09-13 22:28:59 +0200

    lists03 done

---
+++ week-02/day-03/lists03/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+namespace AppendLetter
+{
+    public class AppendLetter
+    {
+        public static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            for (int i = 0; i < far.Count; i++)
+            {
+                far[i] += "a";
+
+                Console.WriteLine(far[i]);
+            }
+            
+  
+
+            
+            Console.ReadLine();
+        }
+    }
+}
