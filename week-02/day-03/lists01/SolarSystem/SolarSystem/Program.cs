01de72aca6e9b424c777066520b38f4682edf479
Lorand <szilagyi.lori@gmail.com>
2017-09-13 16:38:00 +0200

    lists01 done

---
+++ week-02/day-03/lists01/SolarSystem/SolarSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem
+{
+    public class SolarSystem
+    {
+        public static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+
+            planetList.Insert(5 , "Saturn");
+
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine(planet);
+                
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
