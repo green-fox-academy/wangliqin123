840fc59d1770ec30fdb74977d564705f247b5bdd
Lorand <szilagyi.lori@gmail.com>
2017-09-13 12:36:51 +0200

    strings03 done

---
+++ week-02/day-03/strings03/TakesLonger/TakesLonger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TakesLonger
+{
+    public class TakesLonger
+    {
+        public static void Main(string[] args)
+        {
+            string quote = "Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.";
+
+            quote = quote.Insert(21, "always takes longer than ");
+
+            Console.WriteLine(quote);
+            Console.ReadLine();
+        }
+    }
+}
