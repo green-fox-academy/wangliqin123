7f913df28b8ff4dd3a9940d20bfa339fa20430a5
Lorand <szilagyi.lori@gmail.com>
2017-09-14 11:12:06 +0200

    lists05 done

---
+++ week-02/day-03/lists05/ElementFinder/ElementFinder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementFinder
+{
+    public class ElementFinder
+    {
+        public static void Main(string[] args)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+
+            if (List.Contains(7))
+            {
+                Console.WriteLine("Hooray");
+            }
+            else
+            {
+                Console.WriteLine("Noooooo");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
