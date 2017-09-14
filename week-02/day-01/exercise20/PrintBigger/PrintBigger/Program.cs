a2a5484f0b020f2b4d00292eadcb5ed452f4ae34
Lorand <szilagyi.lori@gmail.com>
2017-09-14 15:49:52 +0200

    exercise20 done

---
+++ week-02/day-01/exercise20/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number 1: ");
+            int numb = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Number 2: ");
+            int numb2 = Int32.Parse(Console.ReadLine());
+
+            if (numb > numb2)
+            {
+                
+                Console.WriteLine(numb);
+            }
+
+            else
+            {
+                Console.WriteLine(numb2);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
