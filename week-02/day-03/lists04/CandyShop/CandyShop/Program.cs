09d0b5f95999283c93871f91c85c6869ff903e7c
Lorand <szilagyi.lori@gmail.com>
2017-09-14 11:00:43 +0200

    lists04 done

---
+++ week-02/day-03/lists04/CandyShop/CandyShop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyShop
+{
+    public class Candyshop
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+        
+            list[list.IndexOf(2)] = "Croissont";
+            list[list.IndexOf(false)] = "Ice cream";
+
+            foreach (object sweet in list)
+            {
+                Console.WriteLine(sweet);
+            }
+        
+            Console.ReadLine();
+        }
+    }
+}
