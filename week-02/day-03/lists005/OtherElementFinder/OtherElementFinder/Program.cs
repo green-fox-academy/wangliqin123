6fec6d859082281f8c7131c45cf071fe9f6ca0e7
Lorand <szilagyi.lori@gmail.com>
2017-09-14 11:52:10 +0200

    list005 done

---
+++ week-02/day-03/lists005/OtherElementFinder/OtherElementFinder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementFinder
+{
+    public class ElementFinder
+    {
+        public static void Main(string[] args)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+
+            if (List.IndexOf(7) > 0)
+            {
+                Console.WriteLine("Hoorray");
+            }
+            else
+            {
+                Console.WriteLine("Noooooo");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
