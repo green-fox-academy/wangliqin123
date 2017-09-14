c5883c5d1a33a9f0e52b054e31634f5e92d7aaf4
Lorand <szilagyi.lori@gmail.com>
2017-09-14 12:53:15 +0200

    lists06 done

---
+++ week-02/day-03/lists06/IsInList/IsInList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsInList
+{
+    public class IsInList
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16 };
+
+            if (list.Contains(4) && list.Contains(8) && list.Contains(12) && list.Contains(16))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            Console.ReadLine();  
+        }
+    }
+}
---
+++ week-02/day-03/lists07/QuoteSwap/QuoteSwap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteSwap
+{
+    public class QuoteSwap
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is to fix it by swapping the right words with code
+
+            // Also, print the sentence to the output with spaces in between.
+        }
+    }
+}
+url = (url.Replace("bots" , "odds"));
