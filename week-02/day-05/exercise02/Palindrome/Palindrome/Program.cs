f2f37e840ea52eca0d09fe51f0ac0c7580743f05
Lorand <szilagyi.lori@gmail.com>
2017-09-15 12:11:07 +0200

    exercise02 done

---
+++ week-02/day-05/exercise02/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a word: ");
+            string word= Console.ReadLine();
+
+            string isPalindrome = CreatePalindrome(word);
+            Console.WriteLine(isPalindrome);
+            Console.ReadLine();
+        }
+        public static string CreatePalindrome(string word)
+        {
+            string isPalindrome = word;
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                isPalindrome = isPalindrome + word[i];
+            }
+            return (isPalindrome);
+        }
+    }
+}
