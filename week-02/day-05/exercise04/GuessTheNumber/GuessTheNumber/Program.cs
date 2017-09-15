d9ebbed256f0e102fb8644df1f11cfe7838fcf4d
Lorand <szilagyi.lori@gmail.com>
2017-09-15 13:54:46 +0200

    exerise04 part of it

---
+++ week-02/day-05/exercise04/GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.Write("Enter the starting limit: ");
+            int value1 = int.TryParse(Console.ReadLine());
+
+            Console.Write("Enter the final limit: ");
+            int value2 = int.TryParse(Console.ReadLine());
+
+            int value = random.Next(value1, value2);
+            int guess = 0;
+            bool correct = false;
+
+            Console.WriteLine("I've the number between 1-100.");
+
+            while (!correct)
+            {
+                Console.Write("Guess: ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out guess))
+                {
+                    continue;
+                }
+                if (guess < value)
+                {
+                    Console.WriteLine("Too low.");
+                }
+                else if (guess > value)
+                {
+                    Console.WriteLine("Too high.");
+                }
+                else
+                {
+                    correct = true;
+                    Console.WriteLine("Congratulations. You won!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}   
