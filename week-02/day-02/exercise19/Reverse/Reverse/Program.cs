e8412d369e7dda6ebd9c234b3a87cd0d0e14c5e1
Lorand <szilagyi.lori@gmail.com>
2017-09-17 12:09:28 +0200

    exercise19 done

--- week-02/day-02/exercise19/Reverse/Reverse/Program.cs
+++ week-02/day-02/exercise19/Reverse/Reverse/Program.cs
@@ -6,8 +6,15 @@ namespace Reverse
     {
         static void Main(string[] args)
         {
-            int[] aj = { 3, 4, 5, 6, 7 };
-            
+            int[] aj = {3, 4, 5, 6, 7};
+
+            Array.Reverse(aj);
+
+            foreach (var numb in aj)
+	        {
+                Console.Write("{0}," , numb);
+	        }
+
             Console.ReadLine();
         }
     }
