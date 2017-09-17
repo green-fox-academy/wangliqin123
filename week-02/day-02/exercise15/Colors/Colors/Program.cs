5a3b826afee000aac7cb8d8594f63b9852e5e6c4
Lorand <szilagyi.lori@gmail.com>
2017-09-17 11:27:27 +0200

    exercise15 done

--- week-02/day-02/exercise15/Colors/Colors/Program.cs
+++ week-02/day-02/exercise15/Colors/Colors/Program.cs
@@ -6,15 +6,19 @@ namespace Colors
     {
         static void Main(string[] args)
         {
-            // - Create a two dimensional array
-            //   which can contain the different shades of specified colors
-            // - In `colors[0]` store the shades of green:
-            //   `"lime", "forest green", "olive", "pale green", "spring green"`
-            // - In `colors[1]` store the shades of red:
-            //   `"orange red", "red", "tomato"`
-            // - In `colors[2]` store the shades of pink:
-            //   `"orchid", "violet", "pink", "hot pink"`
-            Console.ReadLine();
+            string[][] colors = new string[3][];
+            colors [0] = new string[5] { "lime", "forest green", "olive", "pale green", "spring green" };
+            colors[1] = new string[3] { "orange red", "red", "tomato" };
+            colors[2] = new string[4] { "orchid", "violet", "pink", "hot pink" };
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                for (int j = 0; j < colors[i].Length; j++)
+                {
+                    Console.WriteLine("" + colors[i][j]);
+                }
+            }
+                Console.ReadLine();  
         }
     }
 }
