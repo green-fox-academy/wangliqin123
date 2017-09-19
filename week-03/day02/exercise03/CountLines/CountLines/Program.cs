cae80026d13ea1a83fb96b6fb42513163c1ff75a
Lorand <szilagyi.lori@gmail.com>
2017-09-19 21:38:51 +0200

    exercise03 done

---
+++ week-03/day02/exercise03/CountLines/CountLines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineCount;
+            string filename;
+
+            Console.Write("Enter a filename: ");
+            filename = Console.ReadLine();
+            lineCount = TakesName(filename);
+
+            Console.WriteLine(lineCount);
+            Console.ReadLine();
+        }
+
+        public static int TakesName(string filename)
+        {
+            int lineCount = 0;
+            try
+            {
+                String[] content = File.ReadAllLines(filename);
+                lineCount = content.Count();
+            } catch (Exception)
+            {}
+            return lineCount;
+        }
+    }
+}
