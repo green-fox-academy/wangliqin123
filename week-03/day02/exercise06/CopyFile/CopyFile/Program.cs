51766da2fcabc62b6daab5f057e5c0657abaecc1
Lorand <szilagyi.lori@gmail.com>
2017-09-19 22:56:54 +0200

    exercise06 basic

---
+++ week-03/day02/exercise06/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = "your-file.txt";
+            string target = "my-file.txt";
+            
+            ReadCopy(source, target);
+            Console.ReadLine();
+        }
+
+        private static void ReadCopy(string source, string target)
+        {
+            using (StreamWriter writer = new StreamWriter(target))
+            {
+                writer.WriteLine(File.ReadAllLines(source));
+            }
+            if (true)
+            {
+
+            }
+        } 
+    }
+    // It should return a boolean that shows if the copy was successful
+}
