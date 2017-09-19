d869de1c4377d01e0ae9f2763eb8ba0665c5d2f0
Lorand <szilagyi.lori@gmail.com>
2017-09-19 12:54:02 +0200

    exercise02 done

---
+++ week-03/day02/exercise02/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "/my-file.txt";
+
+            try
+            {
+                Console.WriteLine(File.ReadAllLines(path));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt"); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
