df6daf57d81bdcaf579fa80bd7ff783970213a79
Lorand <szilagyi.lori@gmail.com>
2017-09-20 11:22:03 +0200

    exercise01 done

---
+++ week-03/day03/exercise01/MIddleLines/MIddleLines/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace MIddleLines
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(150, 0, 150, 300);
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, 150, 300, 150);
+        }  
+    }
+}
