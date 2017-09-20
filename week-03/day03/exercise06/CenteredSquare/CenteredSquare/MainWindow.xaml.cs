37282e58205e4a53e7aab1bbe08794cf4f8184b2
Lorand <szilagyi.lori@gmail.com>
2017-09-20 13:59:41 +0200

    exercise06 done

---
+++ week-03/day03/exercise06/CenteredSquare/CenteredSquare/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace CenteredSquare
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            var points = new List<Point>();
+            points.Add(new Point(40, 70));
+            points.Add(new Point(50, 70));
+            points.Add(new Point(50, 80));
+            points.Add(new Point(40, 80));
+
+            foxDraw.FillColor(Colors.Green);
+            foxDraw.DrawPolygon(points);
+        }
+    }
+}
