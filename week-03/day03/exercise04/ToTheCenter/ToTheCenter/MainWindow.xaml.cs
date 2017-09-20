48fd73e25e634a046c7a873e732b02d40de632f7
Lorand <szilagyi.lori@gmail.com>
2017-09-20 13:15:56 +0200

    exercise04 done

---
+++ week-03/day03/exercise04/ToTheCenter/ToTheCenter/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace ToTheCenter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            LineDrawer(foxDraw, 40, 70);
+            LineDrawer(foxDraw, 120, 80);
+            LineDrawer(foxDraw, 230, 90);
+        }
+
+        public void LineDrawer(FoxDraw Line, int a, int b)
+        {
+            Line.StrokeColor(Colors.Green);
+            Line.DrawLine(new Point(a, b), new Point(canvas.Height/2, canvas.Width/2));
+        }  
+    }
+}
