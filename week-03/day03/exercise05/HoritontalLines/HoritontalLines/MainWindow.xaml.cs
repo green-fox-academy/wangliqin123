07b3f9a65b8102dbfddfec93ef64a6291468df57
Lorand <szilagyi.lori@gmail.com>
2017-09-20 13:36:10 +0200

    exercise05 done

---
+++ week-03/day03/exercise05/HoritontalLines/HoritontalLines/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace HoritontalLines
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            DrawHorizontalLine(foxDraw, 40, 70);
+            DrawHorizontalLine(foxDraw, 100, 130);
+            DrawHorizontalLine(foxDraw, 200, 180);
+        }
+
+        public void DrawHorizontalLine(FoxDraw Line, int x, int y)
+        {
+            Line.StrokeColor(Colors.Green);
+            Line.DrawLine(new Point (x, y), new Point (x + 50, y));
+        }
+    }
+}
