ff603c1089c658922d86db740a822894a4d463a6
Lorand <szilagyi.lori@gmail.com>
2017-09-21 11:27:03 +0200

    exercise02 done

---
+++ week-03/day04/exercise02/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 20;
+
+            Console.WriteLine(NumberAdder(number));
+            Console.ReadLine();
+        }
+
+        public static int NumberAdder(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return number + NumberAdder(number - 1);
+            }
+        }
+    }
+}
