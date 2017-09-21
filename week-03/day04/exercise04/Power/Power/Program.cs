f876bc799c1163adf0133d60ce7e5081d7ec1009
Lorand <szilagyi.lori@gmail.com>
2017-09-21 13:08:10 +0200

    exercise04 done

---
+++ week-03/day04/exercise04/Power/Power/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNum = 3;
+            int n = 3;
+            Console.WriteLine(Power(baseNum, n));
+            Console.ReadLine();
+        }
+
+        public static int Power(int baseNum, int n)
+        {
+            if (n == 0)
+            {
+                return 1; 
+            }
+            else
+            {
+                return baseNum * Power(baseNum, n - 1);
+            }
+        }
+    }
+}
