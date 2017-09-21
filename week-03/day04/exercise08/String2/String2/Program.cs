2fdac001bce311d5a2e1002b899180d8ab372282
Lorand <szilagyi.lori@gmail.com>
2017-09-21 17:32:31 +0200

    exercise08 basic

---
+++ week-03/day04/exercise08/String2/String2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "text";
+            Console.WriteLine(ChangeLetter(word));
+            Console.ReadLine();
+        }
+
+        public static string ChangeLetter(string word)
+        {
+            if (word.Length > 0)
+            {
+                return word[0].ToString().Replace('x', 'y') + ChangeLetter(word.Substring(1));
+            }
+            else
+            {
+                return word;
+            }
+        }
+    }
+}
