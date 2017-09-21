0bea211d815884b7fd83a9ae222864314d00e0c2
Lorand <szilagyi.lori@gmail.com>
2017-09-21 10:20:49 +0200

    exercise01 done

---
+++ week-03/day04/exercise01/Counter/Counter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int number = 200;
+
+        Console.WriteLine(Counter(number));
+        Console.ReadLine();
+        }
+
+        public static int Counter(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Counter(number - 1);
+            }
+        }
+    }
+}
