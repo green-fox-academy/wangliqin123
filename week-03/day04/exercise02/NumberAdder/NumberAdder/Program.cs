37993c7e13520ae7d1ed341ea08aa5a9ca484151
Lorand <szilagyi.lori@gmail.com>
2017-09-21 12:03:16 +0200

    exercise03 done

--- week-03/day04/exercise02/NumberAdder/NumberAdder/Program.cs
+++ week-03/day04/exercise02/NumberAdder/NumberAdder/Program.cs
@@ -28,4 +28,8 @@ namespace NumberAdder
             }
         }
     }
+
+    // Given a non-negative int n, return the sum of its digits recursively (no loops). 
+    // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+    // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
 }
---
+++ week-03/day04/exercise03/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 126;
+
+            Console.WriteLine(SumDigit(number));
+            Console.ReadLine();
+        }
+
+        public static int SumDigit(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return number % 10 + SumDigit(number / 10);
+            }
+        }
+    }
+}
