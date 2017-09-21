bd27db6add6f30733dba9d4faca7742baf05eebf
Lorand <szilagyi.lori@gmail.com>
2017-09-21 14:01:00 +0200

    exercise05 done

---
+++ week-03/day04/exercise05/Bunny1/Bunny1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            int bunny = 130;
+            Console.WriteLine(Bunnies(bunny));
+            Console.ReadLine();
+        }
+        public static int Bunnies(int bunny)
+        {
+            if (bunny > 0)
+            {
+                return 2 + Bunnies(--bunny); 
+            }
+            else
+            {
+                return bunny;
+            }
+        }
+    }
+}
