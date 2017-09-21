2e8a6a64811866f2acf882e6cc8da85c078c8358
Lorand <szilagyi.lori@gmail.com>
2017-09-21 16:01:40 +0200

    exercise06

---
+++ week-03/day04/exercise06/Bunny2/Bunny2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bunny = 10;
+
+            Console.WriteLine(Bunnies(bunny));
+            Console.ReadLine();
+        }
+
+        public static int Bunnies(int bunny)
+        {
+            if (bunny == 1)
+            {
+                return 2;
+            }
+            if (bunny % 2 == 0)
+            {
+                return 3 + Bunnies(--bunny);
+            }
+            else
+            {
+                return 2 + Bunnies(--bunny);
+            }
+        }
+    }
+}
