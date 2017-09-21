78fb76cdae405992f7e86f2fbfa461c5a7a4e30d
Lorand <szilagyi.lori@gmail.com>
2017-09-21 23:01:08 +0200

    exercise09 done

---
+++ week-03/day04/exercise09/String3/String3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "xilofon";
+
+            Console.WriteLine(StarSeparator(word));
+            Console.ReadLine();
+        }
+
+        public static string StarSeparator(string word)
+        {
+            if (word.Length > 0)
+            {
+                return word[0] + "*" + StarSeparator(word.Substring(1));
+            }
+            else
+            {
+                return word;
+            }
+        }
+    }
+}
