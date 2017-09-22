3a5b5c190444ffc944ed3fd8c09c440a878d9a68
Lorand <szilagyi.lori@gmail.com>
2017-09-22 11:27:59 +0200

    exercise01 upsidedown

--- week-03/day05/exercise01/Triangle/Triangle/MainWindow.xaml.cs
+++ week-03/day05/exercise01/Triangle/Triangle/MainWindow.xaml.cs
@@ -22,36 +22,36 @@ namespace Triangle
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
-            int level = 4;
+            int level = 6;
 
-            Point top = new Point(150, 0);
-            Point right = new Point(300, 300);
-            Point left = new Point(0, 300);
+            Point left = new Point(0, 0);
+            Point right = new Point(300, 0);
+            Point bottom = new Point(150, 300);
 
-            DrawTriangle(foxDraw, level, top, right, left);
+            DrawTriangle(foxDraw, level, left, right, bottom);
         }
 
-        public static void DrawTriangle(FoxDraw drawer, int level, Point top, Point right, Point left)
+        public static void DrawTriangle(FoxDraw drawer, int level, Point left, Point right, Point bottom)
         {
             if (level == 0)
             {
                 List<Point> pointT = new List<Point>();
-                pointT.Add(top);
-                pointT.Add(right);
                 pointT.Add(left);
+                pointT.Add(right);
+                pointT.Add(bottom);
 
                 drawer.FillColor(Colors.White);
                 drawer.DrawPolygon(pointT);
             }
             else
             {
-                Point left_mid = new Point((top.X + left.X) / 2f, (top.Y + left.Y) / 2f);
-                Point right_mid = new Point((top.X + right.X) / 2f, (top.Y + right.Y) / 2f);
-                Point bottom_mid = new Point((left.X + right.X) / 2f, (left.Y + right.Y) / 2f);
+                Point top_mid = new Point((left.X + right.X) / 2f, (left.Y + right.Y) / 2f);
+                Point right_mid = new Point((right.X + bottom.X) / 2f, (right.Y + bottom.Y) / 2f);
+                Point left_mid = new Point((bottom.X + left.X) / 2f, (bottom.Y + left.Y) / 2f);
 
-                DrawTriangle(drawer, level - 1, top, left_mid, right_mid);
-                DrawTriangle(drawer, level - 1, left_mid, left, bottom_mid);
-                DrawTriangle(drawer, level - 1, right_mid, bottom_mid, right);
+                DrawTriangle(drawer, level - 1, left, top_mid, left_mid);
+                DrawTriangle(drawer, level - 1, top_mid, right, right_mid);
+                DrawTriangle(drawer, level - 1, left_mid, right_mid, bottom);
             }
         }
     }
