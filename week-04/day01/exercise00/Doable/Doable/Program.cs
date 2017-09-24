126143d7397c19dcedb646d8e6c90bb68b9cc872
Lorand <szilagyi.lori@gmail.com>
2017-09-24 15:15:32 +0200

    exercise00 done

---
+++ week-04/day01/exercise00/Doable/Doable/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> tree1 = new List<object>();
+            tree1.Add("oak");
+            tree1.Add("red");
+            tree1.Add(34);
+            tree1.Add("male");
+
+            List<object> tree2 = new List<object>();
+            tree2.Add("beeach");
+            tree2.Add("green");
+            tree2.Add(21);
+            tree2.Add("male");
+
+            List<object> tree3 = new List<object>();
+            tree3.Add("pine");
+            tree3.Add("silver");
+            tree3.Add(56);
+            tree3.Add("female");
+
+            List<object> tree4 = new List<object>();
+            tree4.Add("willow");
+            tree4.Add("yellow");
+            tree4.Add(76);
+            tree4.Add("male");
+
+            List<object> tree5 = new List<object>();
+            tree5.Add("ash");
+            tree5.Add("brown");
+            tree5.Add(12);
+            tree5.Add("female");
+        }
+    }
+}
