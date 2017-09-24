8ee7a82640c84d52a216ff51d67307e4813dc377
Lorand <szilagyi.lori@gmail.com>
2017-09-24 12:49:29 +0200

    exercise11 done

---
+++ week-03/day04/exercise11/YellowLines/YellowLines/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using GreenFox;
+
+namespace YellowLines
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.FillColor(Colors.Yellow);
+
+            int level = 6;
+            double Height = canvas.Height;
+            double Width = canvas.Width;
+            double coordX = 0;
+            double coordY = 0;
+           
+            DrawRecta(foxDraw, level, Height, Width, coordX, coordY);  
+        }
+
+        public static void DrawRecta(FoxDraw drawer, int level, double Height, double Width, double x, double y)
+        {
+            if (level == 0)
+            {
+                return;
+            }
+            else
+            {
+                drawer.DrawRectangle(x, y, Height, Width);
+
+                double wid = Width / 3f;
+                double hgt = Height / 3f;
+                double x1 = x + wid;
+                double y1 = y + hgt;
+                double x2 = x + wid * 2f;
+                double y2 = y + hgt * 2f;
+
+                DrawRecta(drawer, level - 1, wid, hgt, x, y1);
+                DrawRecta(drawer, level - 1, wid, hgt, x1, y);
+                DrawRecta(drawer, level - 1, wid, hgt, x1, y2);
+                DrawRecta(drawer, level - 1, wid, hgt, x2, y1);
+            }
+        }
+    }
+}
