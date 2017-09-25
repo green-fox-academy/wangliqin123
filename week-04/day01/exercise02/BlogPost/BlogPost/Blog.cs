0e27c9259bb6578d87fc63a178551db2bf9a55e8
Lorand <szilagyi.lori@gmail.com>
2017-09-25 13:54:49 +0200

    exercise02 done

---
+++ week-04/day01/exercise02/BlogPost/BlogPost/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost
+{
+    public class Blog
+    {
+        public string AuthorName;
+        public string Title;
+        public string Text;
+        public string PublicationDate;
+    }
+}
---
+++ week-04/day01/exercise02/BlogPost/BlogPost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Blog blog1 = new Blog();
+            blog1.AuthorName = "John Doe";
+            blog1.Title = "Lorem Ipsum";
+            blog1.Text = "Lorem ipsum dolor sit amet.";
+            blog1.PublicationDate = "2000.05.04.";
+
+            Blog blog2 = new Blog();
+            blog2.AuthorName = "Tim Urban";
+            blog2.Title = "Wait but why";
+            blog2.Text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
+            blog2.PublicationDate = "2010.10.10.";
+
+            Blog blog3 = new Blog();
+            blog3.AuthorName = "William Turton";
+            blog3.Title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
+            blog3.Text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. " +
+                         "When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t " +
+                         "really into the whole organizer profile thing.";
+            blog3.PublicationDate = "2017.03.28.";
+        }
+    }
+}
