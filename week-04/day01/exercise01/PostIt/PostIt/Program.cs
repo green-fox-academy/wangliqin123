c6a62f4b3193b74467cd30fa1df87438c43ccffd
Lorand <szilagyi.lori@gmail.com>
2017-09-25 13:21:13 +0200

    exercise01 done

---
+++ week-04/day01/exercise01/PostIt/PostIt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt
+{
+    class Program
+    { 
+        struct PostIt
+        {
+            public string Text;
+            public string TextColor;
+            public string BackGroundColor;
+        }
+
+        static void Main(string[] args)
+        {
+            PostIt postit1;
+            postit1.Text = "Idea1";
+            postit1.TextColor = "blue";
+            postit1.BackGroundColor = "orange";
+
+            PostIt postit2;
+            postit2.Text = "Awesome";
+            postit2.TextColor = "black";
+            postit2.BackGroundColor = "pink";
+
+            PostIt postit3;
+            postit3.Text = "Superb";
+            postit3.TextColor = "green";
+            postit3.BackGroundColor = "yellow";
+        }
+    }
+}
