7940c67e08bf1b64d99c090ccba98e2cbd356bb1
Lorand <szilagyi.lori@gmail.com>
2017-09-27 14:53:32 +0200

    Sum exercise all test run

---
+++ week-04/day03/exercise02/Sum/Sum/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sum
+{
+    public class SumNumb
+    {
+        public int SumElements(List<int> inputNumbers)
+        {
+            if (inputNumbers != null)
+            {
+                return inputNumbers.Sum();
+            }
+            else
+            {
+                return 10;
+            }
+        }
+    }
+}
---
+++ week-04/day03/exercise02/Sum/SumTest/Class1.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using Sum;
+
+namespace SumTest
+{
+    [TestFixture]
+    public class SumTest
+    {
+        [Test]
+        public void SumElementsTest()
+        {
+            List<int> numbs = new List<int> { 2, 3, 4 };
+            SumNumb SumClass= new SumNumb();
+            int output = 9;
+
+            Assert.AreEqual(output, SumClass.SumElements(numbs));
+        }
+
+        [Test]
+        public void SumElementsTestEmpty()
+        {
+            List<int> emptyList = new List<int>();
+            SumNumb SumClass = new SumNumb();
+            int output = 0;
+
+            Assert.AreEqual(output, SumClass.SumElements(emptyList));
+        }
+
+        [Test]
+        public void SumElementsTestOne()
+        {
+            List<int> oneList = new List<int> { 2 };
+            SumNumb SumClass = new SumNumb();
+            int output = 2;
+
+            Assert.AreEqual(output, SumClass.SumElements(oneList));
+        }
+
+        [Test]
+        public void SumElementsTestNull()
+        {
+            SumNumb SumClass = new SumNumb();
+            int output = 10;
+
+            Assert.AreEqual(output, SumClass.SumElements(null));
+        }
+    }
+}
