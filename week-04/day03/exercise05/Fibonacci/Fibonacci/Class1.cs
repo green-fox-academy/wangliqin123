c52a8e91f949221044d5ed1be20940b6e9c0cb49
Lorand <szilagyi.lori@gmail.com>
2017-09-27 17:24:03 +0200

    fibonacci tested

---
+++ week-04/day03/exercise05/Fibonacci/Fibonacci/Class1.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Fibonacci
+{
+    public class FiboClass
+    {
+        public double GetNthFibonacci(int n)
+        {
+            double nthFibonacci = 0;
+            if (n == 2)
+            {
+                nthFibonacci = 1;
+            }
+            if (n <= 2)
+            {
+                return nthFibonacci;
+            }
+
+            double prevPrev = 0;
+            double prev = 1;
+            for (int i = 1; i < n - 1; i++)
+            {
+                nthFibonacci = prevPrev + prev;
+                prevPrev = prev;
+                prev = nthFibonacci;
+            }
+            return nthFibonacci;
+        }
+    }
+}
---
+++ week-04/day03/exercise05/Fibonacci/FibonacciTest/Class1.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Fibonacci;
+
+namespace FibonacciTest
+{
+    [TestFixture]
+    public class FiboClassTest
+    {
+        FiboClass fiboClass = new FiboClass();
+
+        [Test]
+        public void GetNthFibonacciTest()
+        {
+            int n = 2;
+            int output = 1;
+
+            Assert.AreEqual(output, fiboClass.GetNthFibonacci(n));
+        }
+
+        [Test]
+        public void GetNthFibonacciTestFalse()
+        {
+            int n = 2;
+            int output = 2;
+
+            Assert.AreEqual(output, fiboClass.GetNthFibonacci(n));
+        }
+    }
+}
