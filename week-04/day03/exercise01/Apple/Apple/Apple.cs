8baf71ebdb5458660710e24b7d6217c10eb4f2c9
Lorand <szilagyi.lori@gmail.com>
2017-09-27 13:13:03 +0200

    first test succed

---
+++ week-04/day03/exercise01/Apple/Apple/Apple.cs
@@ -0,0 +1,17 @@
+﻿namespace Apple
+{
+    public class Apples
+    {
+        public string fruit;
+
+        public Apples(string fruit)
+        {
+            this.fruit = "";
+        }
+
+        public string GetApple()
+        {
+            return fruit;
+        }
+    }
+}
---
+++ week-04/day03/exercise01/Apple/AppleTest/AppleTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Apple;
+
+namespace AppleTest
+{
+    [TestFixture]
+    public class AppleTest
+    {
+        [Test]
+        public void GetAppleTest()
+        {
+            string fruit = "apple";
+
+            var apples = new Apples(fruit);
+
+            string output = apples.GetApple();
+
+            Assert.AreEqual(fruit, output);
+        }
+
+        [Test]
+        public void GetAppleTestEmpty()
+        {
+            string fruit = "";
+
+            var apples = new Apples(fruit);
+
+            string output = apples.GetApple();
+
+            Assert.AreEqual(fruit, output);
+        }
+    }
+}
