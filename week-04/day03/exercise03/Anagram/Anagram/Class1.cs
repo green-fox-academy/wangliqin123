8d8b53833155723ea6096e1cb51ca4c26ada3168
Lorand <szilagyi.lori@gmail.com>
2017-09-27 15:37:55 +0200

    anagram tests works

---
+++ week-04/day03/exercise03/Anagram/Anagram/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Anagram
+{
+    public class AnagramClass
+    {
+        public bool IsAnagram(string firstword, string secondword)
+        {
+            if (firstword.Length != secondword.Length)
+            {
+                return false;
+            }
+            char[] first = firstword.ToCharArray();
+            char[] second = secondword.ToCharArray();
+
+            Array.Sort(first);
+            Array.Sort(second);
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ week-04/day03/exercise03/Anagram/AnagramTest/Class1.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class AnagramClassTest
+    {
+        AnagramClass anaCLass = new AnagramClass();
+
+        [Test]
+        public void IsAnagramTest()
+        {
+            string first = "silver";
+            string second = "livers";
+
+            bool output = true;
+
+            Assert.AreEqual(output, anaCLass.IsAnagram(first, second));
+        }
+
+        [Test]
+        public void IsAnagramTestNotEqual()
+        {
+            string first = "silver";
+            string second = "liver";
+
+            bool output = false;
+
+            Assert.AreEqual(output, anaCLass.IsAnagram(first, second));
+        }
+
+        [Test]
+        public void IsAnagramTestNotSameLetters()
+        {
+            string first = "silver";
+            string second = "qliver";
+
+            bool output = false;
+
+            Assert.AreEqual(output, anaCLass.IsAnagram(first, second));
+        }
+    }
+}
