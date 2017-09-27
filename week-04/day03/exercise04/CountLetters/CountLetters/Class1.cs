b761d58381d842147491f6d2d7616f70dbf28e2d
Lorand <szilagyi.lori@gmail.com>
2017-09-27 17:06:30 +0200

    dictionary test works

---
+++ week-04/day03/exercise04/CountLetters/CountLetters/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CountLetters
+{
+    public class Count
+    {
+        public Dictionary<char, int> DictionaryMaker(string word)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            word.ToCharArray();
+           
+            foreach (var letter in word)
+            {
+                if (!map.ContainsKey(letter))
+                {
+                    map.Add(letter, +1);
+                }
+                else
+                {
+                    map[letter]++;
+                }  
+            }
+            return map;
+        }
+    }
+}
---
+++ week-04/day03/exercise04/CountLetters/CountLettersTest/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using CountLetters;
+using NUnit.Framework;
+
+namespace CountLettersTest
+{
+    [TestFixture]
+    public class CountTest
+    {
+        Count countClass = new Count();
+
+        [Test]
+        public void DictionaryMakerTest()
+        {
+            string word = "lotto";
+            Dictionary<char, int> output = new Dictionary<char, int>()
+            {
+                { 'l', 1},
+                { 'o', 2},
+                { 't', 2}
+            };
+
+            Assert.AreEqual(output, countClass.DictionaryMaker(word));
+        }
+
+        [Test]
+        public void DictionaryMakerTestSingle()
+        {
+            string word = "lot";
+            Dictionary<char, int> output = new Dictionary<char, int>()
+            {
+                { 'l', 1},
+                { 'o', 1},
+                { 't', 1}
+            };
+
+            Assert.AreEqual(output, countClass.DictionaryMaker(word));
+        }
+    }
+}
