81d0f19b79f866ced9fe57684948a811191b94f7
Lorand <szilagyi.lori@gmail.com>
2017-10-02 12:15:15 +0200

    six passed test in anagram exercise

---
+++ week-05/day01/exercise01/Anagram/Anagram/AnagramClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Anagram
+{
+    public class AnagramClass
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool IsAnagram(string input1, string input2)
+        {
+            string input1Low = input1.ToLower();
+            string input2Low = input2.ToLower();
+            char[] charArray1 = input1Low.ToCharArray();
+            char[] charArray2 = input2Low.ToCharArray();
+            Array.Sort(charArray1);
+            Array.Sort(charArray2);
+
+            if (input1.Length != input2.Length || input1 == string.Empty || input2 == string.Empty)
+            {
+                return false;
+            }
+            string newInput1 = new string(charArray1);
+            string newInput2 = new string(charArray2);
+            if (newInput1 == newInput2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ week-05/day01/exercise01/Anagram/AnagramTest/AnagramTestClass.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using System;
+using Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture]
+
+    class AnagramTestClass
+    {
+        AnagramClass anagramClass = new AnagramClass();
+
+        static void Main(string[] args)
+        {
+        }
+
+        [Test]
+        public void TestAnagram_WhenInputAlmaAmla_ThenTrue()
+        {
+            string input1 = "alma";
+            string input2 = "amla";
+            bool output = true;
+
+            Assert.AreEqual(output, anagramClass.IsAnagram(input1, input2));
+        }
+
+        [Test]
+        public void TestAnagram_WhenInputAlmaKorte_ThenFalse()
+        {
+            string input1 = "alma";
+            string input2 = "korte";
+            bool output = false;
+
+            Assert.AreEqual(output, anagramClass.IsAnagram(input1, input2));
+        }
+
+        [Test]
+        public void TestAnagram_WhenInputEmpty_ThenFalse()
+        {
+            string input1 = String.Empty;
+            string input2 = String.Empty;
+            bool output = false;
+
+            Assert.AreEqual(output, anagramClass.IsAnagram(input1, input2));
+        }
+
+        [Test]
+        public void TestAnagram_WhenInputAlmaAaml_ThenTrue()
+        {
+            string input1 = "alma";
+            string input2 = "aaml";
+            bool output = true;
+
+            Assert.AreEqual(output, anagramClass.IsAnagram(input1, input2));
+        }
+        [Test]
+        public void TestAnagram_WhenInputalmaAlma_ThenTrue()
+        {
+            string input1 = "alma";
+            string input2 = "Alma";
+            bool output = true;
+
+            Assert.AreEqual(output, anagramClass.IsAnagram(input1, input2));
+        }
+
+        [Test]
+        public void TestAnagram_WhenInputAlmaAaaa_ThenFalse()
+        {
+            string input1 = "alma";
+            string input2 = "aaaa";
+            bool output = false;
+
+            Assert.AreEqual(output, anagramClass.IsAnagram(input1, input2));
+        }
+    }
+}
