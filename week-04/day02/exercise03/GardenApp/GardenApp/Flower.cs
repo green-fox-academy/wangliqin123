2c3f553c6e2bddca890a254c4d962b40f3865056
Lorand <szilagyi.lori@gmail.com>
2017-10-03 22:35:01 +0200

    garden app classes added

--- week-04/day02/exercise03/GardenApp/GardenApp/Flower.cs
+++ week-04/day02/exercise03/GardenApp/GardenApp/Flower.cs
@@ -6,28 +6,16 @@ using System.Threading.Tasks;
 
 namespace GardenApp
 {
-    class Flower
+    class Flower : Plant
     {
         
         int flowerWaterAmount;
         double waterAbsorb = 0.75;
-    
 
         public Flower()
         {
             this.flowerWaterAmount = 3;
         }
 
-        public void FlowerWatering(int flowerWaterAmount)
-        {
-            if (flowerWaterAmount < 5)
-            {
-            Console.WriteLine("Flower needs water");
-            }
-            else
-            {
-                    return (flowerWaterAmount + waterAbsorb);
-            }
-        }
     }
 }
--- week-04/day02/exercise03/GardenApp/GardenApp/Plant.cs
+++ week-04/day02/exercise03/GardenApp/GardenApp/Plant.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GardenApp
 {
-    class Plant
+    class Plant : Garden
     {
         string treeColor = "purple";
         string treeColorOther = "orange";
