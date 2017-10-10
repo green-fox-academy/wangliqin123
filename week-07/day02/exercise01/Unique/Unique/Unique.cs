f65039892bec75a0eb0320a0912fb4670811c0f6
Lorand <szilagyi.lori@gmail.com>
2017-10-10 19:36:15 +0200

    first task unique done

---
+++ week-07/day02/exercise01/Unique/Unique/Unique.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Unique
+{
+    public class Unique
+    {
+        public static char[] UniqueCharacters(string inputWord)
+        {
+            char[] letters = inputWord.Replace(" ", "").ToLower().ToCharArray().Distinct().ToArray();
+
+            foreach (var character in letters)
+            {
+                Console.Write("\"{0}\", ", character);
+            }
+            return letters;
+        }
+    }
+}
---
+++ week-07/day02/exercise01/Unique/Unique/UniqueTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Unique
+{
+    [TestFixture]
+    class UniqueTest
+    {
+        [Test]
+        public void UniqueCharactersTest()
+        {
+            string inputWord = "cat";
+            char[] output = { 'c', 'a', 't' };
+
+            Assert.AreEqual(output, Unique.UniqueCharacters(inputWord));
+        }
+
+        [Test]
+        public void UniqueCharactersTestIsUpper()
+        {
+            string inputWord = "Cat";
+            char[] output = { 'c', 'a', 't' };
+
+            Assert.AreEqual(output, Unique.UniqueCharacters(inputWord));
+        }
+
+        [Test]
+        public void UniqueCharactersTestWhenInsideSpace()
+        {
+            string inputWord = "ca t";
+            char[] output = { 'c', 'a', 't' };
+
+            Assert.AreEqual(output, Unique.UniqueCharacters(inputWord));
+        }
+    }
+}
