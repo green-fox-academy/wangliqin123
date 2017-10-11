d2a98cbedaac0f997e6c0e94a7a41e71cd078d7b
Lorand <szilagyi.lori@gmail.com>
2017-10-11 20:33:48 +0200

    animals finally works

--- week-07/day02/exercise02/Animals/Animals/Commands.cs
+++ week-07/day02/exercise02/Animals/Animals/Commands.cs
@@ -1,68 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace Animals
 {
     class Commands
     {
-        private string path;
-        private string newAnimal;
-        private string[] text;
-        private string line;
-        private string word;
-
-        public Commands()
-        {
-            path = @"./favourites.txt";
-            word = "animal";          
-        }
-
-        public Commands(string newAnimal, string line, string[] text)
-        {
-            this.newAnimal = newAnimal;
-            this.line = line;
-            this.text = text;
-        }
+        private string path = @"./favourites.txt";
+        private string text;
 
         public void Usage()
         {
-            Console.WriteLine("```C# FavouriteAnimals [{0}] [{1}]```", word, word);
+            Console.WriteLine("```C# FavouriteAnimals [animal] [animal]```");
         }
 
-        public void ListAnimals()
+        public void AddAnimal(string[] args)
         {
-            text = File.ReadAllLines(path);
+            text = File.ReadAllText(path);
 
-            foreach (var line in text)
+            using (StreamWriter writer = new StreamWriter(path, true))
             {
-                Console.WriteLine(line);
-            }
-        }
-
-        public void RemoveAnimal(string line)
-        {
-            List<string> lines = new List<string>(File.ReadAllLines(path));
-            lines.RemoveAt(int.Parse(line) - 1);
-            File.WriteAllLines(path, lines);
-        }
-
-        public void AddAnimal(string newAnimal)
-        {
-            text = File.ReadAllLines(path);
-
-            if (text.Contains(newAnimal))
-            {
-                Console.WriteLine("Cannot add beacause already exist.");
-            }
-            else
-            {
-                using (StreamWriter writer = new StreamWriter(path, true))
+                for (int i = 0; i < args.Length; i++)
                 {
-                    writer.WriteLine(newAnimal);
-                }
-            }          
+                    if (!text.Contains(args[i]))
+                    {
+                        writer.WriteLine(args[i]);
+                    }                                        
+                }                    
+            }           
         }
     }
 }
--- week-07/day02/exercise02/Animals/Animals/Program.cs
+++ week-07/day02/exercise02/Animals/Animals/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Animals
+﻿using System.Collections.Generic;
+
+namespace Animals
 {
     class Program
     {
@@ -6,23 +8,13 @@
         {
             Commands commands = new Commands();
 
-            string[] arguments = { "-r", "-a", "-l" };
-
-            if (args.Length == 0)
+            if (args.Length > 0)
+            {
+                commands.AddAnimal(args);            
+            }         
+            else
             {
                 commands.Usage();
-            }
-            else if (args[0] == "-l")
-            {
-                commands.ListAnimals();
-            }
-            else if (args[0] == "-r")
-            {
-                commands.RemoveAnimal(args[1]);
-            }
-            else if (args[0] == "-a")
-            {
-                commands.AddAnimal(args[1]);
             }
         }
     }
