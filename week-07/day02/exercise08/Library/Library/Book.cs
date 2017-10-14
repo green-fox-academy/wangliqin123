dea031f184aebb3f992e1a7ba7b1c22836136593
Lorand <szilagyi.lori@gmail.com>
2017-10-14 10:06:08 +0200

    library classes added

---
+++ week-07/day02/exercise08/Library/Library/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        public string author;
+        public string title;
+        public int releaseYear;
+
+        public Book()
+        {
+            this.author = author;
+            this.title = title;
+            this.releaseYear = releaseYear;
+
+        }
+    }
+}
---
+++ week-07/day02/exercise08/Library/Library/BookShelf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class BookShelf
+    {
+        internal void toString()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void put(string v1, string v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void remove(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
