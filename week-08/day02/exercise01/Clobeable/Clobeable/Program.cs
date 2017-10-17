4af63352683674ecd9af6ccc0b666dfe612c5a08
Lorand <szilagyi.lori@gmail.com>
2017-10-17 13:51:58 +0200

    feat(clobeadle) new object cloned succesfully

---
+++ week-08/day02/exercise01/Clobeable/Clobeable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clobeable
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student john = new Student("John Doe", 20, "male", "BME");
+            Student johnTheClone = (Student)john.Clone();
+            john.Introduce();
+            johnTheClone.Introduce();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-08/day02/exercise01/Clobeable/Clobeable/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clobeable
+{
+    class Student : ICloneable
+    {
+        string name;
+        int age;
+        string gender;
+        string previousOrganization;
+
+        public Student(string name, int age, string gender, string previousOrganization)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.previousOrganization = previousOrganization;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I'm " + name + ", a " + age + " year old " + gender + " from " +
+                             previousOrganization + " who skipped " + " days from the course already.");
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
