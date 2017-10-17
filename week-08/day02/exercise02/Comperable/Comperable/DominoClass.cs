44e1509e2bdc95dae297dee179f3ada2e148ab12
Lorand <szilagyi.lori@gmail.com>
2017-10-17 17:09:13 +0200

    comperable add method

---
+++ week-08/day02/exercise02/Comperable/Comperable/DominoClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Comperable
+{
+    public class Domino : IComparable
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int CompareTo(object obj)
+        {
+            Domino domino = obj as Domino;
+            return domino.GetValues()[1].CompareTo(domino.GetValues()[0]);
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+    }
+}
