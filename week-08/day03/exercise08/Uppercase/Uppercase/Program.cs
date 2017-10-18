b9bf037660eb178075fc630923642c6c9ae4632d
Lorand <szilagyi.lori@gmail.com>
2017-10-18 15:02:36 +0200

    uppercase done

---
+++ week-08/day03/exercise08/Uppercase/Uppercase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Uppercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "cAt";
+
+            var findUpper = from character in word.ToArray()
+                            where Char.IsUpper(character)
+                            select character;
+
+            var findUpperWithLambda = word.Where(x => char.IsUpper(x));
+
+            foreach (var item in findUpperWithLambda)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
