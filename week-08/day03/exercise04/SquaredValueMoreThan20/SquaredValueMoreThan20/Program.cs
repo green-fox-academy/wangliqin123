cfde07590dffed73bafb289c40330b42625ec2c6
Lorand <szilagyi.lori@gmail.com>
2017-10-18 12:40:33 +0200

    squaredvasluemorethan20 works

---
+++ week-08/day03/exercise04/SquaredValueMoreThan20/SquaredValueMoreThan20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaredValueMoreThan20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+            var squares = from number in n
+                          where number * number > 20
+                          select number;
+
+            var squaresWithLambda = n.Where(x => x * x > 20).Select(x => x);
+
+            foreach (var number in squaresWithLambda)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
