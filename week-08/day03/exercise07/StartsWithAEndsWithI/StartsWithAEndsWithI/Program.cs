d71fdc1825567b1e69a7af8a6471263fd3648964
Lorand <szilagyi.lori@gmail.com>
2017-10-18 14:47:55 +0200

    startswithi works

---
+++ week-08/day03/exercise07/StartsWithAEndsWithI/StartsWithAEndsWithI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace StartsWithAEndsWithI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var startsA = from word in cities
+                          where word.StartsWith("A")
+                          where word.EndsWith("I")
+                          select word;
+
+            var startsAWithLambda = cities.Where(x => x.StartsWith("A")).Where(x => x.EndsWith("I"));
+
+            foreach (var item in startsAWithLambda)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
