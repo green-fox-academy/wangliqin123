ed0a1ecff5e052a2ff25453941f57cbae1f36228
Lorand <szilagyi.lori@gmail.com>
2017-10-18 12:04:02 +0200

    fix(square) add quary and lambda solution

---
+++ week-08/day03/exercise03/Squared/Squared/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Squared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var positives = from positiveNumber in n
+                             where positiveNumber > 0
+                             select positiveNumber * positiveNumber;          
+
+            var positivesWithLambda = n.Where(x => x > 0).Select(x => x * x);
+
+            foreach (var number in positives)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
