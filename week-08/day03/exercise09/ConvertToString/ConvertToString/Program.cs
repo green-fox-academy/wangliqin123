1abc5d539aee6ec9be7414f6489a78f272aab4ec
Lorand <szilagyi.lori@gmail.com>
2017-10-18 17:25:13 +0200

    add linq solution in convertotstring

--- week-08/day03/exercise09/ConvertToString/ConvertToString/Program.cs
+++ week-08/day03/exercise09/ConvertToString/ConvertToString/Program.cs
@@ -10,10 +10,14 @@ namespace ConvertToString
         {
             char[] letters = { 'c', 'a', 't' };
 
+            var toStringQuery = String.Concat(
+                                from character in letters
+                                select character);
+
             var toStringWithLambda = String.Join("", letters.Select(x => x.ToString()));
             var toStringLambda = letters.Aggregate(new StringBuilder(), (a, b) => a.Append(b));
 
-
+            Console.WriteLine(toStringQuery);
             Console.WriteLine(toStringWithLambda);
             Console.WriteLine(toStringLambda);
 
