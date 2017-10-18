1a0e28826711b7544fc8fd3f39a8ea0eb8cf64b4
Lorand <szilagyi.lori@gmail.com>
2017-10-18 14:23:53 +0200

    frequencystring works

---
+++ week-08/day03/exercise06/FrequencyString/FrequencyString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace FrequencyString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = "letter";
+
+            var frequencyOfCharacters = from character in n                                    
+                                     group character by character into chars
+                                     select new { chars.Key, Count = chars.Count() };
+
+            var frequency = n.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var item in frequency)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
