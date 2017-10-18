ddecc8b600b21481d698583bad9c9733f863a0b3
Lorand <szilagyi.lori@gmail.com>
2017-10-18 16:17:28 +0200

    converrtostring only with lambda

---
+++ week-08/day03/exercise09/ConvertToString/ConvertToString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ConvertToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = { 'c', 'a', 't' };
+
+            var toStringWithLambda = String.Join("", letters.Select(x => x.ToString()));
+            var toStringLambda = letters.Aggregate(new StringBuilder(), (a, b) => a.Append(b));
+
+
+            Console.WriteLine(toStringWithLambda);
+            Console.WriteLine(toStringLambda);
+
+            Console.ReadLine();
+        }
+    }
+}
