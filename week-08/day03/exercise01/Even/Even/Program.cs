0d395d2a527f3978fe0640ac9b11712ef0250652
Lorand <szilagyi.lori@gmail.com>
2017-10-18 11:46:13 +0200

    odds and evenavergare works

---
+++ week-08/day03/exercise01/Even/Even/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var evenNumbers = from evenNumb in n
+                              where evenNumb % 2 == 0
+                              select evenNumb;
+
+            var evenNumbersWithLambda = n.Where(x => x % 2 == 0);
+
+            foreach (var even in evenNumbersWithLambda)
+            {
+                Console.WriteLine(even);   
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-08/day03/exercise02/OddAverage/OddAverage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var oddAverage = from oddAverageNumb in n
+                             where oddAverageNumb % 2 != 0
+                             select oddAverageNumb;
+            Console.WriteLine(oddAverage.Average());
+
+            var oddAverageWithLambda = n.Where(x => x % 2 != 0).Average();
+
+            Console.WriteLine(oddAverageWithLambda);
+            Console.ReadLine();
+        }
+    }
+}
