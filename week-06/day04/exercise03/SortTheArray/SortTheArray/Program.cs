a2b8b81d96060d78c73a3ed36c60215c4dc31c60
Lorand <szilagyi.lori@gmail.com>
2017-10-22 13:40:09 +0200

    feat(SortTheArray) works, but i dont know as it describe

---
+++ week-06/day04/exercise03/SortTheArray/SortTheArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortArray(new int[] { 10, 13, 7, 5, 2, 0, 5 });
+            SortArray(args);
+
+            Console.ReadLine();
+        }
+
+        public static void SortArray<T>(T[] array)
+        {
+            array = array.OrderByDescending(x => x).ToArray();
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
