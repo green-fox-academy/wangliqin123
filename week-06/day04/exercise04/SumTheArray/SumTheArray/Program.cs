cf159cb0bbc8220a848f94fa5e810ce36ae753f3
Lorand <szilagyi.lori@gmail.com>
2017-10-22 13:57:09 +0200

    feat(SumTheArray add new array and method's boilerplate

---
+++ week-06/day04/exercise04/SumTheArray/SumTheArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumArray(new int[] { 1, 2, 3, 4 });
+            SumArray(args);
+
+            Console.ReadLine();
+        }
+
+        public static void SumArray<T>(T[] array)
+        {
+            array.Sum();
+        }
+    }
+}
