8e8838d5f5bdf45ccee435c77a21f50e2ba9aaaf
Lorand <szilagyi.lori@gmail.com>
2017-10-22 09:03:37 +0200

    style(Clobeable): delete usings

--- week-08/day02/exercise01/Clobeable/Clobeable/Program.cs
+++ week-08/day02/exercise01/Clobeable/Clobeable/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Clobeable
 {
@@ -14,7 +10,6 @@ namespace Clobeable
             Student johnTheClone = (Student)john.Clone();
             john.Introduce();
             johnTheClone.Introduce();
-
             Console.ReadLine();
         }
     }
--- week-08/day02/exercise01/Clobeable/Clobeable/Student.cs
+++ week-08/day02/exercise01/Clobeable/Clobeable/Student.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Clobeable
 {
@@ -23,8 +19,7 @@ namespace Clobeable
 
         public void Introduce()
         {
-            Console.WriteLine("Hi, I'm " + name + ", a " + age + " year old " + gender + " from " +
-                             previousOrganization + ".");
+            Console.WriteLine("Hi, I'm " + name + ", a " + age + " year old " + gender + " from " + previousOrganization + ".");
         }
 
         public object Clone()
