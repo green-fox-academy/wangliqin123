8585f57f66eb662fa233768bbf14c7b013bb09e8
Lorand <szilagyi.lori@gmail.com>
2017-10-24 18:57:45 +0200

    feat(firstapllication) hellos have random size and color, also add randomly changing background-color

--- week-07/day02/exercise01/FirstApplication/FirstApplication/Models/Greeting.cs
+++ week-07/day02/exercise01/FirstApplication/FirstApplication/Models/Greeting.cs
@@ -1,4 +1,6 @@
-﻿namespace FirstApplication.Models
+﻿using System;
+
+namespace FirstApplication.Models
 {
     public class Greeting
     {
@@ -8,5 +10,6 @@
                 "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
                 "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
                 "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+        public Random random = new Random();
     }
 }
