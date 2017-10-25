e83447d9b4f0f2061aa06b04678642bc943b4669
Lorand <szilagyi.lori@gmail.com>
2017-10-25 11:15:33 +0200

    style(firstapllication) delete some empty line

--- week-07/day02/exercise01/FirstApplication/FirstApplication/Controllers/WebController.cs
+++ week-07/day02/exercise01/FirstApplication/FirstApplication/Controllers/WebController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using FirstApplication.Models;
 
 namespace FirstApplication.Controllers
@@ -19,9 +15,7 @@ namespace FirstApplication.Controllers
             {
                 Id = ++counter,
                 Content = name,
-
             };
-
             return View(greeting);
         }
     }
