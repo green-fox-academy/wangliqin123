cfc36f2c50194ef4824ea7eb20b746c725028eb3
Lorand <szilagyi.lori@gmail.com>
2017-10-25 12:02:39 +0200

    feat(bankofsimba): add classes and folders

---
+++ week-07/day03/exercise01/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; } 
+        public double Balance { get; set; } 
+        public string AnimalType { get; set; }
+
+    }
+}
---
+++ week-07/day03/exercise01/BankOfSimba/BankOfSimba/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BankOfSimba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.ViewModels
+{
+    public class HomeViewModel
+    {
+        BankAccount bankAccount = new BankAccount();
+    }
+}
