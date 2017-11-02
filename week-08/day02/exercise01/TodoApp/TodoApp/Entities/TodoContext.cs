e785763226feb26bba2a74dd166c22a99e2f9a3e
Lorand <szilagyi.lori@gmail.com>
2017-11-02 21:03:24 +0100

    style(TodoApp) delete useless lines and usings

--- week-08/day02/exercise01/TodoApp/TodoApp/Entities/TodoContext.cs
+++ week-08/day02/exercise01/TodoApp/TodoApp/Entities/TodoContext.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TodoApp.Entities
 {
@@ -10,7 +6,6 @@ namespace TodoApp.Entities
     {
         public TodoContext(DbContextOptions<TodoContext> options) : base(options)
         {
-
         }
 
         public DbSet<Todo> Todos { get; set; }
