9667c56f2f531f93d807662df82435d07011dfaf
Lorand <szilagyi.lori@gmail.com>
2017-11-07 12:19:10 +0100

    feat(Frontend) setup done

---
+++ week-09/day02/exercise01/Frontend/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [Route("/doubling")]
+        [HttpGet]
+        public IActionResult Index(int? input)
+        {
+
+
+            return Json();
+        }
+    }
+}
