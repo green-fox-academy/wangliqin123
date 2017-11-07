626545a75dba4fa38076a5cef5aa7710020f1a8c
Lorand <szilagyi.lori@gmail.com>
2017-11-07 17:02:02 +0100

    feat(dountil) with postman test ok

--- week-09/day02/exercise01/Frontend/Frontend/Controllers/HomeController.cs
+++ week-09/day02/exercise01/Frontend/Frontend/Controllers/HomeController.cs
@@ -45,7 +45,69 @@ namespace Frontend.Controllers
             {
                 return Json(new { welcome_message = $"Oh, hi there {name}, my dear {title}!" });
             }
-
         }
+
+        [Route("/appenda/{appendable}")]
+        [HttpGet]
+        public IActionResult Appenda(string appendable)
+        {
+            return Json(new {appended = $"{appendable}a" });
+        }
+
+        [Route("/dountil/{what}")]
+        [HttpPost]
+        public IActionResult Index(string what, [FromBody] DoUntil until)
+        {
+
+            if (until == null)
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+            else if (what == "sum")
+            {
+                int sum = 0;
+
+                for (int i = 0; i <= until.Until; i++)
+                {
+                    sum += i;
+                }
+                return Json(new {result = sum});
+            }
+            else if(what == "factor")
+            {
+                int fact = 1;
+                fact = until.Until;
+                for (int i = until.Until - 1; i >= 1; i--)
+                {
+                    fact = fact * i;
+                }
+                return Json(new {result = fact});
+            }
+            else
+            {
+                return Json(new {  result = 4 });
+            }
+        }
+
+        public class DoUntil
+        {
+            public int Until { get; set; }
+        }
+
+        [HttpPost]
+        [Route("/arrays")]
+        public IActionResult Array([FromBody] MyArray numbers)
+        {
+            if ()
+            {
+
+            }
+        }
+
+        public class MyArray
+        {
+            public int[] numbers { get; set; }
+        }
+
     }
 }
