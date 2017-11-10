b425d7349be34504fc1ad7cf698745aeff3f68b9
Lorand <szilagyi.lori@gmail.com>
2017-11-10 12:05:33 +0100

    feat(TodoApp): add xunit test to update, delete functions, all passed

--- week-08/day02/exercise01/TodoApp/TodoAppIntegrationTests/TodoAppShould.cs
+++ week-08/day02/exercise01/TodoApp/TodoAppIntegrationTests/TodoAppShould.cs
@@ -37,7 +37,7 @@ namespace TodoAppIntegrationTests
         [Fact]
         public async Task AddNewTodo()
         {
-            var options = new DbContextOptionsBuilder<TodoContext>().UseInMemoryDatabase(databaseName: "TodosTestDb").Options;
+            var options = new DbContextOptionsBuilder<TodoContext>().UseInMemoryDatabase(databaseName: "TodosAddTestDb").Options;
 
             using (var dbContext = new TodoContext(options))
             {
@@ -45,14 +45,101 @@ namespace TodoAppIntegrationTests
 
                 dbContext.Todos.Add(new Todo()
                 {
-                    Title = "make lunch",                  
+                    Title = "make lunch",
                 });
                 dbContext.SaveChanges();
 
                 string expected = "make lunch";
                 var todo = await dbContext.Todos.FirstOrDefaultAsync(x => x.Title.Equals("make lunch"));
                 Assert.Equal(expected, todo.Title);
-            }             
+            }
+        }
+
+        [Fact]
+        public void DeleteTodo()
+        {
+            var options = new DbContextOptionsBuilder<TodoContext>().UseInMemoryDatabase(databaseName: "TodosDelTestDb").Options;
+
+            using (var dbContext = new TodoContext(options))
+            {
+                var todoRepository = new TodoRepository(dbContext);
+
+                dbContext.Todos.Add(new Todo()
+                {
+                    Id = 100,
+                });
+                dbContext.SaveChanges();
+
+                var deletedTodo = dbContext.Todos.FirstOrDefault(x => x.Id.Equals(100));
+                todoRepository.DeleteTodo(deletedTodo.Id);
+                dbContext.SaveChanges();
+
+                var expected = 0;
+                var numberOfTodos = dbContext.Todos.Count();
+                Assert.Equal(expected, numberOfTodos);
+            }
+        }
+
+        [Fact]
+        public async Task UpdateTodo()
+        {
+            var options = new DbContextOptionsBuilder<TodoContext>().UseInMemoryDatabase(databaseName: "TodosUpdateTestDb").Options;
+
+            using (var dbContext = new TodoContext(options))
+            {
+                var todoRepository = new TodoRepository(dbContext);
+
+                dbContext.Todos.Add(new Todo()
+                {
+                    Id = 99,
+                    Title = "make breakfast",
+                    IsDone = false,
+                    IsUrgent = true
+                });
+                dbContext.SaveChanges();
+
+
+
+                var updateTodo = dbContext.Todos.FirstOrDefault(x => x.Id.Equals(99));
+                updateTodo.Title = "make supper";
+                todoRepository.UpdateTodo(updateTodo);
+                dbContext.SaveChanges();
+
+                string expected = "make supper";
+                var todo = await dbContext.Todos.FirstOrDefaultAsync(x => x.Id.Equals(99));
+                Assert.Equal(expected, todo.Title);
+            }
+        }
+
+        [Fact]
+        public void DeleteTodoWithAddTwoElementAndRemainsOne()
+        {
+            var options = new DbContextOptionsBuilder<TodoContext>().UseInMemoryDatabase(databaseName: "TodosAddTwoTestDb").Options;
+
+            using (var dbContext = new TodoContext(options))
+            {
+                var todoRepository = new TodoRepository(dbContext);
+
+                dbContext.Todos.Add(new Todo()
+                {
+                    Id = 100,
+                });
+                dbContext.SaveChanges();
+
+                dbContext.Todos.Add(new Todo()
+                {
+                    Id = 101,
+                });
+                dbContext.SaveChanges();
+
+                var deletedTodo = dbContext.Todos.FirstOrDefault(x => x.Id.Equals(100));
+                todoRepository.DeleteTodo(deletedTodo.Id);
+                dbContext.SaveChanges();
+
+                var expected = 1;
+                var numberOfTodos = dbContext.Todos.Count();
+                Assert.Equal(expected, numberOfTodos);
+            }
         }
     }
 }
