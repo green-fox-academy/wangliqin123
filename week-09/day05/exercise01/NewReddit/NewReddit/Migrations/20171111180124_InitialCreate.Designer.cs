13580f1001400def62949d9ef5f0f130cf33e30a
Lorand <szilagyi.lori@gmail.com>
2017-11-11 19:04:04 +0100

    feat(NewReddit) add packages and update database

---
+++ week-09/day05/exercise01/NewReddit/NewReddit/Migrations/20171111180124_InitialCreate.Designer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NewReddit.Entities;
+
+namespace NewReddit.Migrations
+{
+    [DbContext(typeof(RedditContext))]
+    [Migration("20171111180124_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ChangeDetector.SkipDetectChanges", "true")
+                .HasAnnotation("ProductVersion", "1.1.3")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
---
+++ week-09/day05/exercise01/NewReddit/NewReddit/Migrations/RedditContextModelSnapshot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NewReddit.Entities;
+
+namespace NewReddit.Migrations
+{
+    [DbContext(typeof(RedditContext))]
+    partial class RedditContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ChangeDetector.SkipDetectChanges", "true")
+                .HasAnnotation("ProductVersion", "1.1.3")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
