b6b9d66154ad77f08092d9a2d3d71774f0db1bec
Lorand <szilagyi.lori@gmail.com>
2017-11-11 20:34:10 +0100

    feat(NewReddit) add folders

---
+++ week-09/day05/exercise01/NewReddit/NewReddit/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewReddit.Models
+{
+    public class Post
+    {
+        public string Request { get; set; }
+        public string Response { get; set; }
+    }
+}
